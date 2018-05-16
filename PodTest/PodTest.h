fdb006d3a56db565c9769a759617403d8d4e8600
周明亮 <mingliang.zhou@bblink.cn>
2018-05-16 10:42:09 +0800

    提交项目

---
+++ PodTest/PodTest.h
@@ -0,0 +1,15 @@
+//
+//  PodTest.h
+//  PodTest
+//
+//  Created by 周明亮 on 2018/5/16.
+//  Copyright © 2018年 BBlink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodTest : NSObject
+
++(void)test;
+
+@end
---
+++ PodTest/PodTest.m
@@ -0,0 +1,17 @@
+//
+//  PodTest.m
+//  PodTest
+//
+//  Created by 周明亮 on 2018/5/16.
+//  Copyright © 2018年 BBlink. All rights reserved.
+//
+
+#import "PodTest.h"
+
+@implementation PodTest
+
++(void)test{
+    NSLog(@"hello world");
+}
+
+@end
